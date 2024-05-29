b73eae60358e3f23047982ee255100400cc80608
katia <katia.lecam@gmail.com>
2024-05-29 13:49:23 +0200

    style css

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\RecetteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class AccueilController extends AbstractController
 {
-    #[Route('/', name: 'app_accueil')]
-    public function index(): Response
+    #[Route('/', name: 'app_accueil', methods:['GET'])]
+    public function index(RecetteRepository $recette,): Response
     {
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
+
+            //Récuperation des 6 dernières recettes
+            'recette'=>$recette->findBy(
+                [],
+                ['id'=>'DESC'],
+                9)
         ]);
     }
 }
